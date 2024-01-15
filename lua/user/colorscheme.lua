4ea6766034f6e467c5afc1fb63619a2e4d82b091
abdullah.muhammad.bajwa@gmail.com <muhamad.a.bajwa@gmail.com>
2024-01-15 14:56:48 -0800

    Update colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,12 @@
-local M = {
-  "Mofiqul/dracula.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+local M =
+{
+    "miikanissi/modus-themes.nvim",
+    priority = 1000,
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme}
 }
 
 function M.config()
-  vim.cmd.colorscheme "dracula"
+    vim.cmd.colorscheme "modus_vivendi"
 end
 
 return M
--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup {
   spec = LAZY_PLUGIN_SPEC,
   install = {
-    colorscheme = { "dracula" },
+    colorscheme = { "modus_vivendi" },
   },
   ui = {
     border = "rounded",
